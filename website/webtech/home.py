4c97ab02f41eb42e4641e1f67427ad13c50df3f7
gavinvanderbij <g.van.der.bij@st.hanze.nl>
2025-03-27 16:01:59 +0100

    Templates added

--- website/webtech/home.py
+++ website/webtech/home.py
@@ -1,10 +1,25 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def index():
-    # render de template Basic.html
-    return "<h1>Weddingplanner Blooming Days.</h1>"
+def home():
+    #Homepagina/Over ons/nav bar naar andere paginas.
+    return render_template("home.html")
+
+@app.route("/diensten")   
+def diensten():
+    #Dienstenpagina (wat leveren ze, link naar contactpagina)
+    return render_template("diensten.html") 
+
+@app.route("/contact") 
+def contact():
+    #contactpagina (contactmogelijk + afspraken maken)
+    return render_template("contact.html")
+
+@app.route("/login")
+def login():
+    #loginpagina voor databases
+    return render_template("contact.html")
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
