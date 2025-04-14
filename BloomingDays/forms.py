c6dbe14e647dbbeb8185451029e4ae53a8609adf
Anh-Thy04 <anhthytran2309@gmail.com>
2025-04-14 17:29:23 +0200

    map made and added needed files

--- BloomingDays/forms.py
+++ BloomingDays/forms.py
@@ -2,7 +2,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Email, EqualTo
 from wtforms import ValidationError
-from models import User
+from BloomingDays.models import User
 
 class LoginForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
