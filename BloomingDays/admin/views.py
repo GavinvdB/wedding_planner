a43d585c31118ac641cafcb2fe66f18d1e7ffb3e
Anh-Thy04 <anhthytran2309@gmail.com>
2025-04-16 15:55:44 +0200

    updates htmls

--- BloomingDays/admin/views.py
+++ BloomingDays/admin/views.py
@@ -1,6 +1,8 @@
 from flask import Blueprint, Flask, render_template, redirect, url_for, session, flash, request
 from BloomingDays import db
 from flask_login import login_user, login_required, logout_user
+from BloomingDays.admin.models import User
+from BloomingDays.forms import LoginForm
 
 admin_blueprint = Blueprint('admin',
                              __name__,
@@ -21,4 +23,23 @@ def welkom():
 def logout():
     logout_user()
     flash('Je bent nu uitgelogd!')
-    return redirect(url_for('home')) 
+    return redirect(url_for('home'))
+
+user_blueprint = Blueprint('user',
+                            __name__,
+                            template_folder='templates')
+
+@user_blueprint.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+
+        user = User.query.filter_by(email=form.email.data).first()
+        
+        if user and user.check_password(form.password.data):
+            login_user(user)
+            flash('Gefeliciteerd! Inloggen gelukt!', 'success')
+            return redirect(url_for('admin.welkom'))
+        else:
+            flash('Ongeldige inloggegevens', 'danger')
+    return render_template('login.html', form=form)
