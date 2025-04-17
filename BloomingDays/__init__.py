db3cac5061e0422a40f9e953d518a59068ead47a
gavinvanderbij <g.van.der.bij@st.hanze.nl>
2025-04-17 14:46:24 +0200

    init.py fixed

--- BloomingDays/__init__.py
+++ BloomingDays/__init__.py
@@ -27,7 +27,7 @@ migrate = Migrate(app, db)
 # Configure login
 login_manager = LoginManager()
 login_manager.init_app(app)
-login_manager.login_view = 'login'
+login_manager.login_view = 'user.login'
 
 from BloomingDays.admin.views import admin_blueprint 
 from BloomingDays.user.views import user_blueprint
