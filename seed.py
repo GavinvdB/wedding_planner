3253b36c75079a6732217a85d46c6cfd148bd6af
Anh-Thy04 <anhthytran2309@gmail.com>
2025-04-17 14:33:42 +0200

    update seed.py

--- seed.py
+++ seed.py
@@ -1,9 +1,10 @@
 from BloomingDays import db, app
 from BloomingDays.admin.models import User
 
-adminAcc = User(email="admin@admin.com", username="admin", password="admin", is_admin=True),
+adminAcc = User(email="admin@admin.com", username="admin", password="admin", is_admin=True)
 
 with app.app_context():
+    db.create_all()
     # Remove all existing
     User.query.delete()
     db.session.commit()
